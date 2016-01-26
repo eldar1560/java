062faed3976702ddab3465dcaf12bbfd6e08eb30
Eldar Gabay <eldar1560@walla.com>
2016-01-26 15:40:48 +0200

    updated the java doc in properties

--- src/presenter/Properties.java
+++ src/presenter/Properties.java
@@ -2,6 +2,12 @@ package presenter;
 
 import java.io.Serializable;
 
+/**
+ * the Properties class - contains the variables that the user is giving us from xml
+ * @author Eldar ,Ofek
+ *
+ */
+
 public class Properties implements Serializable{
 	
 	private static final long serialVersionUID = 42L;
@@ -13,7 +19,10 @@ public class Properties implements Serializable{
 	String algorithmForSolution;
 	String algorithmForGenerate;
 	String uc;
-
+	
+	/**
+	 * Constructor for properties - default values
+	 */
 	public Properties() {
 		this.x = 6;
 		this.y = 6;
@@ -24,67 +33,128 @@ public class Properties implements Serializable{
 		this.name = "Maze1";
 		this.uc = "GUI";
 	}
-
+	/**
+	 * get the user choice
+	 * @return
+	 */
 	public String getUc() {
 		return uc;
 	}
-
+	/**
+	 * set the user choice
+	 * @param uc
+	 */
 	public void setUc(String uc) {
 		this.uc = uc;
 	}
-
+	
+	/**
+	 * get the x size
+	 * @return
+	 */
 	public int getX() {
 		return x;
 	}
-
+	
+	/**
+	 * set the x size
+	 * @param x
+	 */
 	public void setX(int x) {
 		this.x = x;
 	}
-
+	
+	/**
+	 * get the y size
+	 * @return
+	 */
 	public int getY() {
 		return y;
 	}
-
+	/**
+	 * set the y size
+	 * @param y
+	 */
 	public void setY(int y) {
 		this.y = y;
 	}
 
+	/**
+	 * get the z size
+	 * @return
+	 */
 	public int getZ() {
 		return z;
 	}
 
+	/**
+	 * set the z size
+	 * @param z
+	 */
 	public void setZ(int z) {
 		this.z = z;
 	}
 
+	/**
+	 * get the number of threads
+	 * @return
+	 */
 	public int getNumberOfThreads() {
 		return numberOfThreads;
 	}
-
+	
+	/**
+	 * set the number of threads
+	 * @param numberOfThreads
+	 */
 	public void setNumberOfThreads(int numberOfThreads) {
 		this.numberOfThreads = numberOfThreads;
 	}
 
+	/**
+	 * get the name of the maze
+	 * @return
+	 */
 	public String getName() {
 		return name;
 	}
-
+	
+	/**
+	 * set the name of the maze
+	 * @param name
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * get the algorithm for solution
+	 * @return
+	 */
 	public String getAlgorithemForSolution() {
 		return algorithmForSolution;
 	}
 
+	/** 
+	 * set the algorithm for solution
+	 * @param algorithmForSolution
+	 */
 	public void setAlgorithemForSolution(String algorithmForSolution) {
 		this.algorithmForSolution = algorithmForSolution;
 	}
 
+	/**
+	 * get the algorithm for generate the maze
+	 * @return
+	 */
 	public String getAlgorithemForGenerate() {
 		return algorithmForGenerate;
 	}
 
+	/**
+	 * set the algorithm for generate the maze
+	 * @param algorithmForGenerate
+	 */
 	public void setAlgorithemForGenerate(String algorithmForGenerate) {
 		this.algorithmForGenerate = algorithmForGenerate;
 	}
